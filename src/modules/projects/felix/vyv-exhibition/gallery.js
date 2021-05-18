4c40b4a66a2ea86670697c26b0e335ebc335a3fb
Filip Antala <fifcostyle@gmail.com>
2021-05-18 19:54:01 +0200

    gallery design and bugfixes

--- src/modules/projects/felix/vyv-exhibition/gallery.js
+++ src/modules/projects/felix/vyv-exhibition/gallery.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Typography, Card, CardHeader, CardContent, CardActions, Button, makeStyles, Divider } from '@material-ui/core';
+import { Typography, Card, CardContent, CardActions, Button, makeStyles, Divider } from '@material-ui/core';
 
-const classes = [
+const clss = [
     {
         id: 'plameniaky',
         name: 'Plameniaky',
@@ -36,31 +36,51 @@ const classes = [
 
 const useStyles = makeStyles(theme => ({
     root: {
+
+    },
+    header: {
+        textAlign: 'center',
+        marginTop: theme.spacing(1),
+        marginBottom: theme.spacing(1),
+    },
+    cards: {
         display: 'flex',
-        flexWrap: 'wrap'
+        flexWrap: 'wrap',
+        justifyContent: 'center',
+    },
+    card: {
+        marginLeft: theme.spacing(1),
     }
 }))
 
 function Gallery() {
     const classes = useStyles();
 
+    console.log(clss);
+
     return (
         <div className={classes.root}>
-            <Typography variant="h3">Triedne galérie:</Typography>
-            {classes.map(cls => (
-                <Card key={cls.id}>
-                    <CardHeader>{cls.name}</CardHeader>
-                    <Divider />
-                    <CardContent>
-                        {cls.desc}
-                    </CardContent>
-                    <CardActions>
-                        <Button color="secondary" variant="contained" href={`/gallery/${cls.id}`}>
-                            Prejsť do galérie
-                        </Button>
-                    </CardActions>
-                </Card>
-            ))}
+            <Typography className={classes.header} variant="h3">Triedne galérie:</Typography>
+            <div className={classes.cards}>
+                {clss.map(cls => (
+                    <Card key={cls.id} className={classes.card} raised>
+                        <CardContent>
+                            <Typography variant="h6" color="textSecondary">
+                                {cls.name}
+                            </Typography>
+                            <Divider />
+                            <Typography>
+                                {cls.desc}
+                            </Typography>
+                        </CardContent>
+                        <CardActions>
+                            <Button color="secondary" variant="contained" href={`/projects/felix/vyv-exhibition/gallery/${cls.id}`}>
+                                Prejsť do galérie
+                            </Button>
+                        </CardActions>
+                    </Card>
+                ))}
+            </div>
         </div>
     )
 }
