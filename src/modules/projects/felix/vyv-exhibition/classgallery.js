66fb3f1a0fd5b8ddd854ccc3571fb30837a6009a
Filip Antala <fifcostyle@gmail.com>
2021-05-18 19:54:41 +0200

    classgallery work

--- src/modules/projects/felix/vyv-exhibition/classgallery.js
+++ src/modules/projects/felix/vyv-exhibition/classgallery.js
@@ -1,18 +1,18 @@
 import { Typography } from '@material-ui/core';
 import React from 'react';
-import {useParams} from 'react-router-dom';
 import fs from 'fs';
+import { useParams } from 'react-router';
 
 function ClassGallery() {
 	const { cid } = useParams();
-	if (!cid) return 'Not found'
-	const imgpaths = fs.readdirSync(`./gallery/${cid}`);
-	const imgs = imgpaths.map(path => (require(path)));
+	if (!cid) return 'Not found';
+	/* const imgpaths = fs.readdirSync(`./gallery/${cid}`);
+	const imgs = imgpaths.map(path => (require(path))); */
 
 	return (
 		<div>
 			<Typography variant="h3">Galéria triedy {cid.substr(0, 1).toUpperCase() + cid.substr(1)}</Typography>
-			{imgs}
+			{/* {imgs} */}
 		</div>
 	)
 }
